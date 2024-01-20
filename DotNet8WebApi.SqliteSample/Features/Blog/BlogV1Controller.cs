6b2ead2f218a96ccc059714f031f53ad59007993
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-01-20 23:21:12 +0630

    [Added] Blog V1

---
+++ DotNet8WebApi.SqliteSample/Features/Blog/BlogV1Controller.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.SqliteSample.Features.Blog
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogV1Controller : ControllerBase
+    {
+    }
+}
